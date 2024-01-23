c84744b82c5b01c53e8de98b867bed5c40c5ea02
Göktuğ Gezer <ggezer780@gmail.com>
2024-01-23 14:51:09 +0300

    Added to-do update function.And added error message when null value is sent.

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ class UI {
       todolist.innerHTML = "Liste boş";
     } else {
       todos.forEach((item) => {
-        result += `<div id="todolist"  class="d-flex flex justify-content-between align-items-center p-3" > <div class="w-75">${item.todolistvalue}</div>
-        <div class="w-25"><button data-id="${item.todolistid}" value="${item.todolistvalue}" class="btn btn-outline-danger w-100 fs-2 my-2 " onclick="UI.clear(event)">Sil</button></div></div>
+        result += `<div id="todolist"  class="d-flex flex justify-content-between align-items-center p-3" > <div>${item.todolistvalue}</div>
+        <div class="d-flex gap-2"><button data-id="${item.todolistid}" onclick="update(event)" class="btn btn-outline-danger w-100 fs-4  my-2 " >Update</button><button data-id="${item.todolistid}" value="${item.todolistvalue}" class="btn btn-outline-danger w-100 fs-2 my-2 " onclick="UI.clear(event)">Sil</button></div></div>
         </div>`;
       });
       todolist.innerHTML = result;
@@ -55,6 +55,27 @@ class UI {
     todos = [];
     UI.display();
     localStorage.removeItem("data"); //!Local storage dan veriyi siler.
+  }
+   //! TODO LİSTESİNİ GÜNCELLEME METODU:  Seçilen todoyu inputtan aldığı value ya göre  günceller.
+  static updateToDoObject(e){
+  e.preventDefault();
+  if(todoinput.value==""){
+  UI.alert("Please enter one todo ");
+  return;
+  }
+
+  todos.forEach((item)=>{
+  if(item.todolistid==e.target.dataset.id){
+    UI.alert(`${item.todolistvalue} updated.`);
+    item.todolistvalue=todoinput.value;
+  }
+  });
+  const data = {
+  todos,
+  };
+  UI.display();
+  localStorage.setItem("data", JSON.stringify(data)); //!Veriyi localStorage'a kaydet
+
   }
   //! KULLANICI BİLDİRİMİ METODU: Kullanıcıya bildirim gösterir.
   static alert(message) {
@@ -82,6 +103,10 @@ todobutton.addEventListener("click", (e) => {
   e.preventDefault();
   let todoid = todos.length + 1;
   let todovalue = todoinput.value;
+  if (todovalue==""){
+  UI.alert("Please enter one todo.");
+  return;
+  }
   const todoslist = new todo(todoid, todovalue);
   todos.push(todoslist);
   UI.display();
@@ -92,3 +117,5 @@ todobutton.addEventListener("click", (e) => {
   };
   localStorage.setItem("data", JSON.stringify(data));
 });
+
+ const update=(e)=>{UI.updateToDoObject(e)};
